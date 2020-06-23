28df1ae4fa818f7899f8dd167ae020eace4115a0
David Damnjanovic <36664588+AbrakaDaba@users.noreply.github.com>
2020-06-23 15:21:37 +0200

    Add files via upload

---
+++ horse-power.js
@@ -0,0 +1,20 @@
+var ks = document.getElementsByClassName("ks")[0];
+var kw = document.getElementsByClassName("kw")[0];
+
+// Promena  Kilowata u Konjske snage
+// 1 kW = 1,35962 KS
+function kwToKs() {
+    ks.value = Math.round(kw.value * 1.35962);
+    if (kw.value * 0 != 0) {
+        ks.value = "Unesite broj!"
+    }
+}
+
+// Promena Konjskih snaga u Kilowate
+// 1KS = 0.7353 KW
+function ksToKw() {
+    kw.value = Math.round(ks.value * 0.7353);
+    if (ks.value * 0 != 0) {
+        kw.value = "Unesite broj!"
+    }
+}
