7ada93fe2a51870b49ed90fece7511ed984a7070
David Damnjanovic <36664588+AbrakaDaba@users.noreply.github.com>
2020-06-24 11:52:13 +0200

    Update horse-power.js

--- horse-power.js
+++ horse-power.js
@@ -2,9 +2,9 @@ var ks = document.getElementsByClassName("ks")[0];
 var kw = document.getElementsByClassName("kw")[0];
 
 // Promena  Kilowata u Konjske snage
-// 1 kW = 1,35962 KS
+// 1 kW = 1,359621617 KS
 function kwToKs() {
-    ks.value = Math.round(kw.value * 1.35962);
+    ks.value = Math.round(kw.value * 1.359621617);
     if (kw.value * 0 != 0) {
         kw.value = ""
         ks.value = ""
@@ -12,9 +12,9 @@ function kwToKs() {
 }
 
 // Promena Konjskih snaga u Kilowate
-// 1KS = 0.7353 KW
+// 1KS = 0,73549875 KW
 function ksToKw() {
-    kw.value = Math.round(ks.value * 0.7353);
+    kw.value = Math.round(ks.value * 0.73549875);
     if (ks.value * 0 != 0) {
         ks.value = "";
         kw.value = "";
